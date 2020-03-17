4d43181449a650d1975919ac52373ade4cb8da3e
Karl Wettin <karl.wettin@wikimedia.se>
2020-03-17 20:00:39 +0100

    initial

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ echo "Starting Mishkal webserver."
 /srv/app/src/mishkal/interfaces/web/mishkal-webserver &
 sleep 10
 echo "Waited 10 seconds for Mishkal webserver to start before starting Mary TTS."
+export GRADLE_USER_HOME=/srv/app/srv/gradle_cache
 cd /srv/app/src/marytts
 ./gradlew run
 
