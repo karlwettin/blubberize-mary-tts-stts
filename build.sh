74f3b625496eb65a1c828dac370755e5e1a4663d
Karl Wettin <karl.wettin@wikimedia.se>
2020-03-17 20:43:35 +0100

    initial

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+# clean up previous builds
+docker rm wikispeech-mary-tts-stts
+docker rmi wikispeech-mary-tts-stts
+
+# clean up gradle from anything but dependencies
+mv src/gradle_cache/caches src/gradle_caches.tmp
+rm -rf src/gradle_cache
+mkdir src/gradle_cache
+mv src/gradle_caches.tmp src/gradle_cache/caches
+
+# build docker
 ~/opt/blubber mary-tts-stts.yaml production | docker build --tag wikispeech-mary-tts-stts --file - .
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+python -m pip install wheel
+python -m pip install pyarabic arramooz-pysqlite qalsadi tashaphyne mysam-tagmanager
+
+
 echo "Starting Mishkal webserver."
-/srv/app/src/mishkal/interfaces/web/mishkal-webserver &
+/srv/app/src/mishkal/interfaces/web/mishkal-webserver.py &
+
 sleep 10
 echo "Waited 10 seconds for Mishkal webserver to start before starting Mary TTS."
 export GRADLE_USER_HOME=/srv/app/srv/gradle_cache
 cd /srv/app/src/marytts
-./gradlew run
+./gradlew run 
 
