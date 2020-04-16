af073008ad6d09cd88e123a767d194e39a5132c4
Karl Wettin <karl.wettin@wikimedia.se>
2020-04-17 00:59:15 +0200

    no powerup test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 echo "Starting Mary TTS STTS."
 export GRADLE_USER_HOME=/srv/mary-tts-stts/src/gradle_user_home
 cd src/marytts
-./gradlew run --no-rebuild
+./gradlew run --no-rebuild -Dmodules.poweronselftest=false
 
 /bin/bash
