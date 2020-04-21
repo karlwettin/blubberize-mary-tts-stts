8871a305edde83ac59eab29ed839135ed98b6640
Karl Wettin <karl.wettin@wikimedia.se>
2020-04-21 16:33:38 +0200

    Start using binary rather than gradle

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 echo "Starting Mary TTS STTS."
 export GRADLE_USER_HOME=/srv/mary-tts-stts/src/gradle_user_home
-cd src/marytts
-./gradlew run --no-rebuild -Dmodules.poweronselftest=false
-
-/bin/bash
+#export MARY_TTS_MISHKAL_URL=http://localhost:8080/
+#cd src/marytts
+#./gradlew run --no-rebuild -Dmodules.poweronselftest=false
+cd src/marytts/build/install/marytts/
+./bin/marytts-server
+#/bin/bash
